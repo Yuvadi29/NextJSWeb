a79cec2d6f75932a914015ce94437b8af8f10142
Yuvadi29 <adityatrivedi612@gmail.com>
2023-02-24 10:25:14 +0530

    Preview Mode Enabled

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import { PreviewSuspense } from "next-sanity/preview";
 
-const page = () => {
-  return (
-    <div>
-        <h1 className='text-4xl'>Welcome to the Blog Page</h1>
+// This will fetch all the blog posts created till date
+const query = groq`
+*[_type == 'post'] {
+  ...,
+  author ->,
+  categories[] ->
+} | order(_createdAt desc)
+`
+
+const HomePage = async () => {
+  if (previewData()) {
+    return <div>
+      <PreviewSuspense
+        fallback={
+          <div role="status">
+            <p className="text-center text-lg animate-pulse text-[#151725]">
+              Loading Preview Data
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query}/>
+      </PreviewSuspense>
     </div>
+  }
+
+  const posts = await client.fetch(query);
+  return (
+    <div>Not in Preview Mode</div>
   )
 }
-
-export default page;
+export default HomePage;
